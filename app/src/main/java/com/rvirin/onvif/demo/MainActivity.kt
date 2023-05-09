551dd0ace5373f90e885c2a50ea139e6a3e1c758
nikolacrnicrni <crnogorac.nikola93@gmail.com>
2023-05-09 18:07:58 +0200

    first commit

---
+++ app/src/main/java/com/rvirin/onvif/demo/MainActivity.kt
@@ -0,0 +1,118 @@
+package com.rvirin.onvif.demo
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.rvirin.onvif.R
+import com.rvirin.onvif.onvifcamera.*
+
+import com.rvirin.onvif.onvifcamera.OnvifRequest.Type.GetStreamURI
+import com.rvirin.onvif.onvifcamera.OnvifRequest.Type.GetProfiles
+import com.rvirin.onvif.onvifcamera.OnvifRequest.Type.GetDeviceInformation
+import com.rvirin.onvif.onvifcamera.OnvifRequest.Type.GetServices
+
+const val RTSP_URL = "com.rvirin.onvif.onvifcamera.demo.RTSP_URL"
+
+/**
+ * Main activity of this demo project. It allows the user to type his camera IP address,
+ * login and password.
+ */
+class MainActivity : AppCompatActivity(), OnvifListener {
+
+    private var toast: Toast? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun requestPerformed(response: OnvifResponse) {
+
+        Log.d("INFO", response.parsingUIMessage)
+
+        toast?.cancel()
+
+        if (!response.success) {
+            Log.e("ERROR", "request failed: ${response.request.type} \n Response: ${response.error}")
+            toast = Toast.makeText(this, "⛔️ Request failed: ${response.request.type} and ${response.error}", Toast.LENGTH_SHORT)
+            toast?.show()
+        }
+        // if GetServices have been completed, we request the device information
+            else if (response.request.type == GetServices) {
+            currentDevice.getDeviceInformation()
+        }
+        // if GetDeviceInformation have been completed, we request the profiles
+        else if (response.request.type == GetDeviceInformation) {
+
+            val textView = findViewById<TextView>(R.id.explanationTextView)
+            textView.text = response.parsingUIMessage
+            toast = Toast.makeText(this, "Device information retrieved 👍", Toast.LENGTH_SHORT)
+            toast?.show()
+
+            currentDevice.getProfiles()
+
+        }
+        // if GetProfiles have been completed, we request the Stream URI
+        else if (response.request.type == GetProfiles) {
+            val profilesCount = currentDevice.mediaProfiles.count()
+            toast = Toast.makeText(this, "$profilesCount profiles retrieved 😎", Toast.LENGTH_SHORT)
+            toast?.show()
+
+            currentDevice.getStreamURI()
+
+        }
+        // if GetStreamURI have been completed, we're ready to play the video
+        else if (response.request.type == GetStreamURI) {
+
+            val button = findViewById<TextView>(R.id.button)
+            button.text = getString(R.string.Play)
+
+            toast = Toast.makeText(this, "Stream URI retrieved,\nready for the movie 🍿", Toast.LENGTH_SHORT)
+            toast?.show()
+        }
+    }
+
+    fun buttonClicked(view: View) {
+
+        // If we were able to retrieve information from the camera, and if we have a rtsp uri,
+        // We open StreamActivity and pass the rtsp URI
+        if (currentDevice.isConnected) {
+            currentDevice.rtspURI?.let { uri ->
+                val intent = Intent(this, StreamActivity::class.java).apply {
+                    putExtra(RTSP_URL, uri)
+                }
+                startActivity(intent)
+            } ?: run {
+                Toast.makeText(this, "RTSP URI haven't been retrieved", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+
+            // get the information type by the user to create the Onvif device
+            val ipAddress = (findViewById<EditText>(R.id.ipAddress)).text.toString()
+            val login = (findViewById<EditText>(R.id.login)).text.toString()
+            val password = (findViewById<EditText>(R.id.password)).text.toString()
+
+            if (ipAddress.isNotEmpty() &&
+                    login.isNotEmpty() &&
+                    password.isNotEmpty()) {
+
+                // Create ONVIF device with user inputs and retrieve camera informations
+                currentDevice = OnvifDevice(ipAddress, login, password)
+                currentDevice.listener = this
+                currentDevice.getServices()
+
+            } else {
+                toast?.cancel()
+                toast = Toast.makeText(this,
+                        "Please enter an IP Address login and password",
+                        Toast.LENGTH_SHORT)
+                toast?.show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/rvirin/onvif/demo/StreamActivity.kt
@@ -0,0 +1,62 @@
+package com.rvirin.onvif.demo
+
+import android.os.Bundle
+import android.view.SurfaceView
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.pedro.vlc.VlcListener
+import com.pedro.vlc.VlcVideoLibrary
+import com.rvirin.onvif.R
+
+/**
+ * This activity helps us to show the live stream of an ONVIF camera thanks to VLC library.
+ */
+class StreamActivity : AppCompatActivity(), VlcListener, View.OnClickListener {
+
+
+    private var vlcVideoLibrary: VlcVideoLibrary? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_stream)
+
+        val surfaceView = findViewById<SurfaceView>(R.id.surfaceView)
+        val bStartStop = findViewById<Button>(R.id.b_start_stop)
+        bStartStop.setOnClickListener(this)
+        vlcVideoLibrary = VlcVideoLibrary(this, this, surfaceView)
+    }
+
+    /**
+     * Called by VLC library when the video is loading
+     */
+    override fun onComplete() {
+        Toast.makeText(this, "Loading video...", Toast.LENGTH_LONG).show()
+    }
+
+    /**
+     * Called by VLC library when an error occured (most of the time, a problem in the URI)
+     */
+    override fun onError() {
+        Toast.makeText(this, "Error, make sure your endpoint is correct", Toast.LENGTH_SHORT).show()
+        vlcVideoLibrary?.stop()
+    }
+
+
+    override fun onClick(v: View?) {
+
+        vlcVideoLibrary?.let { vlcVideoLibrary ->
+
+            if (!vlcVideoLibrary.isPlaying) {
+                val url = intent.getStringExtra(RTSP_URL)
+                vlcVideoLibrary.play(url)
+
+            } else {
+                vlcVideoLibrary.stop()
+
+            }
+        }
+    }
+}
+
